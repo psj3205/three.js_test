c233e41890eb0c4f3899bde43ce7799db722424b
sejinpark <psj3205@gmail.com>
2019-09-14 22:50:54 +0900

    Create Basic scene objects

---
+++ script2.js
@@ -0,0 +1,117 @@
+var scene;
+var camera;
+var renderer;
+
+function onResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function init() {
+    var stats = initStats();
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMapEnabled = true;
+
+    var planeGeometry = new THREE.PlaneGeometry(60, 40, 1, 1);
+    var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 0;
+    plane.position.y = 0;
+    plane.position.z = 0;
+    plane.receiveShadow = true;
+    scene.add(plane);
+
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;
+    camera.lookAt(scene.position);
+
+    var ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    scene.add(ambientLight);
+    
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(-40, 60, -10);
+    spotLight.castShadow = true;
+    scene.add(spotLight);
+
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+
+    var step = 0;
+
+    var controls = new function () {
+        this.rotationSpeed = 0.02;
+        this.numberOfObjects = scene.children.length;
+
+        this.addCube = function () {
+            const cubeSize = Math.ceil((Math.random() * 3));
+            const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+            const cubeMaterial = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
+            const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+            cube.castShadow = true;
+            cube.name = "cube-" + scene.children.length;
+            cube.position.x = -30 + Math.round((Math.random() * planeGeometry.parameters.width));
+            cube.position.y = Math.round((Math.random() * 5));
+            cube.position.z = -20 + Math.round((Math.random() * planeGeometry.parameters.height));
+            scene.add(cube);
+            this.numberOfObjects = scene.children.length;
+        };
+
+        this.removeCube = function () {
+            const allChildren = scene.children;
+            const lastObject = allChildren[allChildren.length - 1];
+            if (lastObject instanceof THREE.Mesh) {
+                scene.remove(lastObject);
+                this.numberOfObjects = scene.children.length;
+            }
+        };
+
+        this.outputObjects = function(){
+            console.log(scene.children);
+        };
+    };
+
+    var gui = new dat.GUI();
+    gui.add(controls, 'rotationSpeed', 0, 0.5);
+    gui.add(controls, 'addCube');
+    gui.add(controls, 'removeCube');
+    gui.add(controls, 'outputObjects');
+    gui.add(controls, 'numberOfObjects').listen();
+
+    renderScene();
+
+    function renderScene() {
+        stats.update();
+
+        scene.traverse(obj => {
+            if (obj instanceof THREE.Mesh && obj != plane) {
+                obj.rotation.x += controls.rotationSpeed;
+                obj.rotation.y += controls.rotationSpeed;
+                obj.rotation.z += controls.rotationSpeed;
+            }
+        });
+
+        requestAnimationFrame(renderScene);
+        renderer.render(scene, camera);
+    }
+
+    function initStats() {
+        var stats = new Stats();
+        stats.setMode(0);
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        document.getElementById("Stats-output").appendChild(stats.domElement);
+        return stats;
+    }
+}
+window.onload = init;
+window.addEventListener('resize', onResize, false);
